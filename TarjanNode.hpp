8eb6eac7caee5d5037ce8a1bf8669cdced9a3e14
Hassan Sajjad <hassan.sajjad069@gmail.com>
2023-03-08 06:29:34 +0500

    first commit.

---
+++ TarjanNode.hpp
@@ -0,0 +1,153 @@
+#ifndef TOPSORT_TARJANNODE_HPP
+#define TOPSORT_TARJANNODE_HPP
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using std::vector, std::set, std::string, std::cout;
+
+template <typename T> class TarjanNode
+{
+    // Following 4 are reset in findSCCS();
+    inline static int index = 0;
+    inline static vector<TarjanNode *> stack;
+
+    // Output
+    inline static vector<T *> cycle;
+    inline static bool cycleExists;
+
+  public:
+    inline static vector<T *> topologicalSort;
+
+    // Input
+    inline static set<TarjanNode> *tarjanNodes;
+
+    mutable set<TarjanNode *> deps;
+
+    explicit TarjanNode(const T *id_);
+
+    // Find Strongly Connected Components
+    static void findSCCS();
+
+    static void checkForCycle();
+
+    const T *const id;
+
+  private:
+    void strongConnect();
+    int nodeIndex;
+    int lowLink;
+    bool initialized = false;
+    bool onStack;
+};
+template <typename T> bool operator<(const TarjanNode<T> &lhs, const TarjanNode<T> &rhs);
+
+template <typename T> TarjanNode<T>::TarjanNode(const T *const id_) : id{id_}
+{
+}
+
+template <typename T> void TarjanNode<T>::findSCCS()
+{
+    index = 0;
+    cycleExists = false;
+    cycle.clear();
+    stack.clear();
+    topologicalSort.clear();
+    for (auto it = tarjanNodes->begin(); it != tarjanNodes->end(); ++it)
+    {
+        auto &b = *it;
+        auto &tarjanNode = const_cast<TarjanNode<T> &>(b);
+        if (!tarjanNode.initialized)
+        {
+            tarjanNode.strongConnect();
+        }
+    }
+}
+
+template <typename T> void TarjanNode<T>::strongConnect()
+{
+    initialized = true;
+    nodeIndex = TarjanNode<T>::index;
+    lowLink = TarjanNode<T>::index;
+    ++TarjanNode<T>::index;
+    stack.emplace_back(this);
+    onStack = true;
+
+    for (TarjanNode<T> *tarjandep : deps)
+    {
+        if (!tarjandep->initialized)
+        {
+            tarjandep->strongConnect();
+            lowLink = std::min(lowLink, tarjandep->lowLink);
+        }
+        else if (tarjandep->onStack)
+        {
+            lowLink = std::min(lowLink, tarjandep->nodeIndex);
+        }
+    }
+
+    vector<TarjanNode<T> *> tempCycle;
+    if (lowLink == nodeIndex)
+    {
+        while (true)
+        {
+            TarjanNode<T> *tarjanTemp = stack.back();
+            stack.pop_back();
+            tarjanTemp->onStack = false;
+            tempCycle.emplace_back(tarjanTemp);
+            if (tarjanTemp->id == this->id)
+            {
+                break;
+            }
+        }
+        if (tempCycle.size() > 1)
+        {
+            for (TarjanNode<T> *c : tempCycle)
+            {
+                cycle.emplace_back(const_cast<T *>(c->id));
+            }
+            cycleExists = true;
+            return;
+        }
+    }
+    topologicalSort.emplace_back(const_cast<T *>(id));
+}
+
+template <typename T> void TarjanNode<T>::checkForCycle()
+{
+    if (cycleExists)
+    {
+        std::cout <<
+                   "There is a Cyclic-Dependency.\n";
+        size_t cycleSize = cycle.size();
+        for (unsigned int i = 0; i < cycleSize; ++i)
+        {
+            if (cycleSize == i + 1)
+            {
+                cout << cycle[i]->getTarjanNodeName() <<
+                           " Depends On " <<cycle[0]->getTarjanNodeName() << "\n";
+            }
+            else
+            {
+                cout << cycle[0]->getTarjanNodeName() <<
+                     " Depends On " <<cycle[0]->getTarjanNodeName() << "\n";
+            }
+        }
+        exit(EXIT_SUCCESS);
+    }
+}
+
+template <typename T> bool operator<(const TarjanNode<T> &lhs, const TarjanNode<T> &rhs)
+{
+    return lhs.id < rhs.id;
+}
+
+template<typename T>
+struct Dependency{
+    set<T*> dependencies;
+    set<T*> dependents;
+    set<T*> allDependencies;
+};
+
+#endif // TOPSORT_TARJANNODE_HPP
---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <set>
+#include "TarjanNode.hpp"
+
+struct Widget;
+using TWT = TarjanNode<Widget>;
+
+struct Widget {
+    inline static set<TWT> tarjanNodes;
+    set<Widget *> allDependencies;
+    set<Widget *> dependencies;
+    set<Widget *> dependents;
+    TWT *tarjanNode = nullptr;
+
+    void addDependency(Widget &dependency) {
+        if (dependencies.emplace(&dependency).second) {
+            dependency.dependents.emplace(this);
+            if (!tarjanNode) {
+                tarjanNode = const_cast<TWT *>(tarjanNodes.emplace(this).first.operator->());
+            }
+            if (!dependency.tarjanNode) {
+                dependency.tarjanNode = const_cast<TWT *>(tarjanNodes.emplace(&dependency).first.operator->());
+            }
+            tarjanNode->deps.emplace(dependency.tarjanNode);
+        }
+    }
+
+    string getTarjanNodeName() {
+        return "";
+    }
+};
+
+
+int main() {
+
+    Widget a, b, c, d, e, f, g;
+
+    a.addDependency(b);
+    a.addDependency(c);
+    c.addDependency(d);
+    c.addDependency(e);
+    c.addDependency(f);
+    f.addDependency(g);
+
+    TWT::tarjanNodes = &Widget::tarjanNodes;
+    TWT::findSCCS();
+    TWT::checkForCycle();
+
+    vector<Widget *> sorted = std::move(TWT::topologicalSort);
+
+    for (Widget *widget: sorted) {
+        for (Widget *dep: widget->dependencies) {
+            widget->allDependencies.emplace(dep);
+            for(Widget *d : dep->allDependencies)
+            {
+                widget->allDependencies.emplace(d);
+            }
+        }
+    }
+    auto k = a.allDependencies;
+}
